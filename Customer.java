940574a9f9f1ff4c0a66e5e704071e4c8bf91bbe
elementfrvr <dakota.mitchem@siu.edu>
2017-01-24 10:26:27 -0600

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,15 @@
+
+public class Customer {
+	private String customerName;
+	//Constructor
+	public Customer(String Name){
+		customerName=Name;
+	}
+	public String getName(){
+		return customerName;
+	  }
+	public void setCustomer(String Name){
+	    customerName=Name;
+	}
+
+}
